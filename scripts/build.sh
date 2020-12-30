3339e510aed9cac259a52eb5f6d6ca389c2b4787
Jay Robinson <me@j4y.co>
2020-12-30 23:30:55 +0000

    bash boilerplate

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-echo "HELLCHICKEN!"
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+function __log_info () {
+	local log_msg="${1}"
+	echo -e "$(date -u +"%Y-%m-%d %H:%M:%S UTC") ::: ${log_msg}" 1>&2
+}
+
+__log_info "HELLCHICKEN!"
+__log_info cheese
