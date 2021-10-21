bcebd6b61b0080b80c25f9d4253e41a0272a86c6
Jay Robinson <me@j4y.co>
2021-10-21 11:51:46 +0100

    rm verbose arg from lambda build

--- apps/api/lambda_functions/build_lambda.sh
+++ apps/api/lambda_functions/build_lambda.sh
@@ -4,7 +4,7 @@ export GO111MODULE=on
 
 build_func() {
     env GOOS=linux \
-	go build -v -ldflags '-d -s -w' -a -tags netgo \
+	go build -ldflags '-d -s -w' -a -tags netgo \
 	-installsuffix netgo \
 	-o "bin/${1}" "${1}/main.go"
 }
