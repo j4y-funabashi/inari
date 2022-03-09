246d6bf398832e95d2ec1cb657f50d6b5ba85aaf
Jay Robinson <jay.robinson@sainsburys.co.uk>
2022-03-09 09:36:20 +0000

    revert exiftool

--- apps/api/lambda_functions/layers/create_exiftool_layer.sh
+++ apps/api/lambda_functions/layers/create_exiftool_layer.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
 export EXIFTOOL_VERSION=12.40
-export PERL_VERSION="5-30"
-
 rm -rf layer
-curl -sS https://shogo82148-lambda-perl-runtime-ap-southeast-2.s3.amazonaws.com/perl-${PERL_VERSION}-runtime.zip > perl.zip
-mkdir layer
-cd layer
-unzip ../perl.zip
-cd ..
-rm perl.zip
-curl https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz | tar -xJ
+curl https://exiftool.org/Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz  --output Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
+tar -xf Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
+rm -rf Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
 mkdir -p layer/bin
 cp Image-ExifTool-${EXIFTOOL_VERSION}/exiftool layer/bin/.
-sed -i "" "1 s/^.*$/#\!\/opt\/bin\/perl -w/" layer/bin/exiftool
+sed -i "1 s/^.*$/#\!\/opt\/bin\/perl -w/" ./layer/bin/exiftool
 cp -r Image-ExifTool-${EXIFTOOL_VERSION}/lib layer/bin/.
 rm -rf Image-ExifTool-${EXIFTOOL_VERSION}
 cd layer
-zip -r layer.zip ./*
+zip -r exiftool.zip ./*
 cd ..
-mv layer/layer.zip .
+mv layer/exiftool.zip .
 rm -rf layer
