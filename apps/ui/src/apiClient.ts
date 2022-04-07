cdd046b13b86b6b859bc815dc6a5fffe20a94788
Jay Robinson <jay.robinson@sainsburys.co.uk>
2022-04-07 13:43:41 +0100

    change urls

--- apps/ui/src/apiClient.ts
+++ apps/ui/src/apiClient.ts
@@ -21,14 +21,14 @@ export type TimelineQuery = () => Promise<timelineResponse>
 export type TimelineMonthQuery = (monthID:string) => Promise<timelineMonthResponse>
 
 export const fetchTimeline:TimelineQuery = async ():Promise<timelineResponse> => {
-    const res = await API.get("photosAPIdev", "/time/", {})
+    const res = await API.get("photosAPIdev", "/months", {})
     console.log(res)
 
     return res as timelineResponse
 }
 
 export const fetchTimelineMonth:TimelineMonthQuery = async (monthID:string):Promise<timelineMonthResponse> => {
-    const res = await API.get("photosAPIdev", "/time/month" + monthID, {})
+    const res = await API.get("photosAPIdev", "/month/" + monthID, {})
     console.log(res)
 
     return res as timelineMonthResponse
