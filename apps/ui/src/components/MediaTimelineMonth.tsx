64e7a1b8682b837b2d2e5a4b713cf1d5ed065b64
JayR <jay.robinson@sainsburys.co.uk>
2022-07-11 18:28:12 +0100

    update ui

--- apps/ui/src/components/MediaTimelineMonth.tsx
+++ apps/ui/src/components/MediaTimelineMonth.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {timelineMonthResponse} from '../apiClient';
-import {Link} from 'react-router-dom';
+import { timelineMonthResponse } from '../apiClient';
+import { Link } from 'react-router-dom';
 
 export interface MediaTimelineMonthProps {
   mediaTimeline: timelineMonthResponse
 }
 
 const MediaTimelineMonth: React.FunctionComponent<MediaTimelineMonthProps> = (props: MediaTimelineMonthProps) => {
-  const {mediaTimeline} = props
+  const { mediaTimeline } = props
 
   const header = (
     <div>
-      <h1>{ mediaTimeline.collection_meta.date }</h1>
-      <small>{ mediaTimeline.collection_meta.media_count }</small>
+      <h1>{mediaTimeline.collection_meta.title}</h1>
+      <small>{mediaTimeline.collection_meta.media_count}</small>
     </div>
   )
   const media = mediaTimeline.media.map((m) => {
