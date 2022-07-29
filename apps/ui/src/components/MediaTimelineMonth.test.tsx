1636c0539fadd97a1d2edd10b9a00c7fa4498dd0
JayR <jay.robinson@sainsburys.co.uk>
2022-07-29 13:01:57 +0100

    group media by day in UI

--- apps/ui/src/components/MediaTimelineMonth.test.tsx
+++ apps/ui/src/components/MediaTimelineMonth.test.tsx
@@ -5,14 +5,32 @@ import { media } from '../apiClient'
 import { BrowserRouter } from 'react-router-dom';
 
 test('renders component', async () => {
-  const testMedia: media = {
+  const testMedia1: media = {
     id: "test-id-1",
     media_src: {
       small: "img-sm.jpg",
       medium: "img-sm.jpg",
       large: "img-sm.jpg",
     },
-    date: "media-dt-123",
+    date: "2022-06-09T22:19:14Z",
+  }
+  const testMedia2: media = {
+    id: "test-id-2",
+    media_src: {
+      small: "img-sm.jpg",
+      medium: "img-sm.jpg",
+      large: "img-sm.jpg",
+    },
+    date: "2022-06-09T23:19:14Z",
+  }
+  const testMedia3: media = {
+    id: "test-id-3",
+    media_src: {
+      small: "img-sm.jpg",
+      medium: "img-sm.jpg",
+      large: "img-sm.jpg",
+    },
+    date: "2022-06-19T23:19:14Z",
   }
 
   const mediaTimeline = {
@@ -22,7 +40,7 @@ test('renders component', async () => {
       type: "timeline_month",
       media_count: 200
     },
-    media: [testMedia]
+    media: [testMedia2, testMedia1, testMedia3]
   }
 
   render(<BrowserRouter><MediaTimelineMonth mediaTimeline={mediaTimeline} /></BrowserRouter>)
--- apps/ui/src/components/MediaTimelineMonth.tsx
+++ apps/ui/src/components/MediaTimelineMonth.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { timelineMonthResponse } from '../apiClient';
 import { Link } from 'react-router-dom';
+import { timelineMonthResponse } from '../apiClient';
+import { format } from 'date-fns'
 
 export interface MediaTimelineMonthProps {
   mediaTimeline: timelineMonthResponse
@@ -9,22 +10,61 @@ export interface MediaTimelineMonthProps {
 const MediaTimelineMonth: React.FunctionComponent<MediaTimelineMonthProps> = (props: MediaTimelineMonthProps) => {
   const { mediaTimeline } = props
 
+  // sort by date
+  mediaTimeline.media.sort(
+    (a, b) => { if (a.date < b.date) { return -1 } return 1 }
+  )
+  // group media to days
+  let dayCollections = new Map<string, timelineMonthResponse>();
+  mediaTimeline.media.forEach((m) => {
+    const dat = new Date(m.date)
+    const datKey = format(dat, 'yyyy-MM-dd')
+    const datTitle = format(dat, 'eee, do MMM')
+    const dayCollection = dayCollections.get(datKey)
+    if (dayCollection) {
+      dayCollection.media.push(m)
+      dayCollection.collection_meta.media_count = dayCollection.media.length
+      dayCollections.set(datKey, dayCollection)
+    } else {
+      const c: timelineMonthResponse = {
+        media: [m],
+        collection_meta: {
+          id: datKey,
+          title: datTitle,
+          type: "timeline_day",
+          media_count: 1
+        }
+      }
+      dayCollections.set(datKey, c)
+    }
+  })
+
+  // render
+  const media = Array.from(dayCollections.values()).map((v) => {
+    const thumbs = v.media.map((m) => {
+      return (
+        <Link key={m.id} to={`/media/${m.id}`}><img src={m.media_src.small} /></Link>
+      )
+    })
+    return (
+      <div key={v.collection_meta.id}>
+        <h2>{v.collection_meta.title}</h2>
+        <div>{thumbs}</div>
+      </div>
+    )
+  })
+
   const header = (
     <div>
       <h1>{mediaTimeline.collection_meta.title}</h1>
       <small>{mediaTimeline.collection_meta.media_count}</small>
     </div>
   )
-  const media = mediaTimeline.media.map((m) => {
-    return (
-      <li key={m.id} ><Link to={`/media/${m.id}`}><img src={`/${m.media_src.small}`} /></Link></li >
-    )
-  })
 
   return (
     <div>
       {header}
-      <ul>{media}</ul>
+      {media}
     </div>
   )
 }
